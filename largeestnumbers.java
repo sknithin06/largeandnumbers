fc6af50cda585903e8a60307622eccc781914767
sknithin06 <nithinsk939@gmail.com>
2021-07-03 20:32:46 +0530

    Large Commit

--- largeestnumbers.java
+++ largeestnumbers.java
@@ -1,7 +1,28 @@
 package Java;
+
+import java.util.Scanner;
+
 public class largeestnumbers {
 	public static void main(String[] args) {
 		System.out.println("Welcome to BasicCoreProgram");
+		
+		Scanner sc= new Scanner(System.in);   
+		System.out.print("Enter a value of a-");  
+		double a=sc.nextDouble();  
+		System.out.print("Enter a value of b-");  
+		double b=sc.nextDouble();  
+		System.out.print("Enter a value of c-");  
+		double c= sc.nextDouble();  
+		
+		if( a > b && a > c) {
+			System.out.println("the largest value is:"+a);
+		}
+		else if (b > c &&  b > c) {
+			System.out.println("the largest value is:"+b);
+		}
+		else {
+			System.out.println("the largest value is"+c);
+		}
 	}
 }
 
